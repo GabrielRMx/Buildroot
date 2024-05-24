c7d300db585ffd41d544e08c23801146c9084064
GabrielRMx <elmaxpc@gmail.com>
2024-05-24 11:48:10 -0400

    mk updates

---
+++ output/.br2-external.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file; DO NOT EDIT.
+#
+
+BR2_EXTERNAL ?= /home/gabriel/BUILDROOT/buildroot.rockchip.ext
+BR2_EXTERNAL_NAMES = 
+BR2_EXTERNAL_DIRS = 
+BR2_EXTERNAL_MKS = 
+
+BR2_EXTERNAL_NAMES += RK3308
+BR2_EXTERNAL_DIRS += /home/gabriel/BUILDROOT/buildroot.rockchip.ext
+BR2_EXTERNAL_MKS += /home/gabriel/BUILDROOT/buildroot.rockchip.ext/external.mk
+export BR2_EXTERNAL_RK3308_PATH = /home/gabriel/BUILDROOT/buildroot.rockchip.ext
+export BR2_EXTERNAL_RK3308_DESC = RK3308
